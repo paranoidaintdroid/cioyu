bcfbbbcc80b89251847792423d3fd74e12597a8d
paranoidaintdroid <paranoidaintdroid@gmail.com>
2026-04-25 12:41:10 +0530

    feat : PRNG-Xoshiro256pp seeding from SplitMix64 #2

--- crates/cioyu_rng/src/lib.rs
+++ crates/cioyu_rng/src/lib.rs
@@ -29,6 +29,44 @@ impl Rng for SplitMix64{
     }
 }
 
+pub struct Xoshiro256pp {
+    state: [u64; 4],
+}
+
+impl Xoshiro256pp {
+    pub fn new(seed :u64) -> Self{
+        let mut rng = SplitMix64::new(seed);
+        Self { 
+            state: [
+                rng.next_u64(),
+                rng.next_u64(),
+                rng.next_u64(),
+                rng.next_u64(),    
+            ], 
+        }
+    }
+}
+
+impl Rng for Xoshiro256pp {
+    fn next_u64(&mut self) -> u64 {
+        let result = (self.state[0]
+            .wrapping_add(self.state[3]))
+            .rotate_left(23)
+            .wrapping_add(self.state[0]);
+
+        let t =self.state[1] << 17;
+        self.state[2] ^= self.state[0];
+        self.state[3] ^= self.state[1];
+        self.state[1] ^= self.state[2];
+        self.state[0] ^= self.state[3];
+
+        self.state[2] ^= t;
+        self.state[3] = self.state[3].rotate_left(45);
+
+        result
+    }
+}
+
 #[cfg(test)]
 mod test{
     use super::*;
@@ -41,5 +79,29 @@ mod test{
         for _ in 0..3{
             assert_eq!(rng_1.next_u64(), rng_2.next_u64());
         }
+    }
+
+    #[test]
+    fn xoshiro_256pp_test(){
+        let seed : u64 = 2;
+        let mut rng_1 = Xoshiro256pp::new(seed);
+        let mut rng_2 = Xoshiro256pp::new(seed);
+
+        for _ in 0..3{
+            assert_eq!(rng_1.next_u64(), rng_2.next_u64());
+        }
+    }
+
+    #[test]
+    fn f64_range_test(){
+        let seed : u64 = 2;
+        let mut rng_sm64 = SplitMix64::new(seed);
+        let mut rng_xsr256pp = Xoshiro256pp::new(seed);
+        
+        let rng_sm64 = rng_sm64.next_f64();
+        let rng_xsr256pp =rng_xsr256pp.next_f64();
+        
+        assert!(rng_sm64 >= 0.0 && rng_sm64 < 1.0);
+        assert!(rng_xsr256pp >= 0.0 && rng_xsr256pp < 1.0);
     }
 }
